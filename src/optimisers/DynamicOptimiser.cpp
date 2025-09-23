1f2dc31e189fa9fa563d41cda5d7885744b47b3e
David-GW <35219153+David-GW@users.noreply.github.com>
2025-09-23 02:34:17 +0100

    Simplified DynamicOptimiser.cpp by defaulting to waiting

--- src/optimisers/DynamicOptimiser.cpp
+++ src/optimisers/DynamicOptimiser.cpp
@@ -5,9 +5,7 @@
 
 #include <algorithm>
 #include <cstddef>
-#include <limits>
 #include <ranges>
-#include <stdexcept>
 #include <utility>
 #include <vector>
 
@@ -15,7 +13,7 @@
 namespace DynamicOptimiser
 {
 	/**
-	* Takes the data on bond returns, and returns {CRF, choices}, where CRF is the optimal cumulative return factor,
+	* Takes the data on bond returns, and returns { CRF, choices }, where CRF is the optimal cumulative return factor,
 	* and choices stores the buying strategy necessary to achieve this.
 	*/
 	std::pair<double, std::vector<InvestmentAction>> optimiseCRF(const BondReturnData& tenorData)
@@ -26,20 +24,20 @@ namespace DynamicOptimiser
 		const std::size_t numTenors = tenorData.tenors().size();
 
 		// monthlyBestCRF[m] will store the optimal cumulative return factor at month  m,
-		// initialised to { 1, –∞, ..., –∞, –∞ } for months 0 to numMonths, since ANY CRF needs to beat the default,
-		// even if negative for example, and so must be compared to –∞.
-		std::vector monthlyBestCRF(numMonths + 1, -std::numeric_limits<double>::infinity());
-		monthlyBestCRF[0] = 1.0;
+		// initialised to { 1.0, ..., 1.0 } for months 0 to numMonths, since we can always achieve a CRF of 1 by waiting,
+		// i.e. not making any purchases.
+		std::vector monthlyBestCRF(numMonths + 1, 1.0);
 
-		// monthlyBestTenor[m] will store the optimal tenor to reach month m (or -1 if none),
-		// initialised to { -1, ..., -1 } for months 0 to numMonths.
-		// Note that monthlyBestTenor[0] will always be -1, since there is no way to reach month 0.
-		std::vector monthlyBestTenor(numMonths + 1, -1);
+		// monthlyBestTenor[m] will store the optimal tenor to reach month m, or whether to wait from the previous month
+		// (represented by 0), initialised to { 0, ..., 0 } for months 0 to numMonths.
+		std::vector monthlyBestTenor(numMonths + 1, 0);
 
 		// For a given month, we work out the best tenor to reach it by calculating potential CRFs from each tenor
 		for (std::size_t currentMonth = 1; currentMonth <= numMonths; ++currentMonth) {
-			double currentMonthBestCRF = -std::numeric_limits<double>::infinity();
-			int bestTenorToReachMonth = -1;
+			// As a minimum, we know we can stay at the same CRF as last month just by waiting.
+			double currentMonthBestCRF = monthlyBestCRF[currentMonth - 1];
+			// As noted, we represent waiting a month with a tenor of length 0.
+			int bestTenorToReachMonth = 0;
 
 			for (std::size_t i = 0; i < numTenors; ++i) {
 				const int currentTenor = tenorData.tenors()[i];
@@ -47,7 +45,9 @@ namespace DynamicOptimiser
 				// Stop if the tenor length exceeds the current month, since then there is no month a bond of that tenor
 				// can purchased for it to be sold in the current month.
 				// Since the tenors are in ascending order, we can break not just continue.
-				if (currentMonth < static_cast<size_t>(currentTenor)) break;
+				if (currentMonth < static_cast<size_t>(currentTenor)) {
+					break;
+				}
 
 				// Get the return factor for the appropriate tenor and month, calculate the resulting CRF,
 				// and check if this is an improvement on the current best.
@@ -61,49 +61,26 @@ namespace DynamicOptimiser
 					bestTenorToReachMonth = currentTenor;
 				}
 			}
-
-			// We need to check whether to buy or wait (i.e. make no purchase in the current month). This might happen if
-			// delaying allows the purchase of a future bond resulting in a chain of higher CRFs than purchasing
-			// in the current month would allow.
-			if (monthlyBestCRF[currentMonth - 1] > currentMonthBestCRF) {
-				currentMonthBestCRF = monthlyBestCRF[currentMonth - 1];
-				bestTenorToReachMonth = 0;
-			}
 			monthlyBestCRF[currentMonth] = currentMonthBestCRF;
-			// This will still be -1 if no path of tenors can reach the current month, or 0 if waiting was the best option.
+			// This will still be 0 if waiting was the best option.
 			monthlyBestTenor[currentMonth] = bestTenorToReachMonth;
-		}
-
-		// Find the month with the highest CRF (this may not necessarily be the last depending on the tenors vs total months).
-		int bestFinalMonth = 0;
-		// As in the initialisation of monthlyBestCRF[m], we use –∞ since ANY CRF must beat this initial value.
-		double bestCRF = -std::numeric_limits<double>::infinity();
-
-		for (std::size_t currentMonth = 0; currentMonth <= numMonths; ++currentMonth) {
-			if (monthlyBestCRF[currentMonth] > bestCRF) {
-				bestCRF = monthlyBestCRF[currentMonth];
-				bestFinalMonth = static_cast<int>(currentMonth);
-			}
 		}
 
 		// CONSTRUCT LIST OF OPTIMAL CHOICES ---------------------------------------------------------------------------
 
 		std::vector<InvestmentAction> optimalChoices;
 
-		// We need to start from the month will the best CRF.
-		int currentMonth = bestFinalMonth;
+		// We need to start from the final month to reconstruct the path of optimal choices.
+		int currentMonth = static_cast<int>(numMonths);
 
 		// This keeps track of how long, if at all, we have been waiting, so rather than returning x 1-month waits,
 		// we can return a single x-month wait.
 		int waitStreak = 0;
 
-		// To construct out the best path, we work backwards until reaching month 0.
+		// To construct the best path, we work backwards until reaching month 0.
 		while (currentMonth > 0) {
 			int bestTenorToReachMonth = monthlyBestTenor[currentMonth];
-			if (bestTenorToReachMonth == -1) {
-				// This should never happen, but is included to avoid the potential for an infinite loop.
-				throw std::runtime_error("Error computing tenor choices");
-			}
+
 			if (bestTenorToReachMonth == 0) {
 				++waitStreak;
 				--currentMonth;
@@ -141,6 +118,6 @@ namespace DynamicOptimiser
 		// Since we have (necessarily) constructed the path backwards, we reverse it for ease of use later.
 		std::ranges::reverse(optimalChoices);
 
-		return { bestCRF, optimalChoices };
+		return { monthlyBestCRF.back(), optimalChoices };
 	}
 }
